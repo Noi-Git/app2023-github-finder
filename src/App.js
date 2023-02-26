7b9f9cf950a3d569e58c4a8cc7c12f7232235d79
Noi-Git <noi.patsin@gmail.com>
2023-02-25 23:22:00 -0500

    create default props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Navbar icon='fab fa-github' title='Noi Github Finder' />
+        <Navbar />
       </div>
     )
   }
