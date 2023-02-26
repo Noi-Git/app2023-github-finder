d14227b2c2cc0484c36da8e05bb49831edc346b9
Noi-Git <noi.patsin@gmail.com>
2023-02-26 16:30:40 -0500

    use axios with async...await to fetch data

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 
 class App extends Component {
-  componentDidMount() {
-    axios
-      .get('https://api.github.com/users')
-      .then((res) => console.log(res.data))
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users')
+    console.log(res.data)
   }
   render() {
     return (
