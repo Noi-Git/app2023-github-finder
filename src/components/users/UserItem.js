465abd3c637f6c06c52501bce3ed1eb300d05e96
Noi-Git <noi.patsin@gmail.com>
2023-02-25 23:55:18 -0500

    create state without constructor

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react'
 
 export class UserItem extends Component {
   // https://api.github.com/users
-  constructor() {
-    super()
-    this.state = {
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    }
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
   }
+
   render() {
     return (
       <div className='card text-center'>
