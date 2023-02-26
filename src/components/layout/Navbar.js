5a9c78a12c0c171b4735b5188b5d0ad893519776
Noi-Git <noi.patsin@gmail.com>
2023-02-26 01:14:21 -0500

    pass PropTypes to use in UserItem

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Navbar = (props) => {
+const Navbar = ({ title, icon }) => {
   return (
     <nav className='navbar bg-primary'>
       <h1>
-        <i className={props.icon} /> {props.title}
+        <i className={icon} /> {title}
       </h1>
     </nav>
   )
