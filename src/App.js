521f7bfac3aa939d85b7697173e5974f6c3ada4f
Noi-Git <noi.patsin@gmail.com>
2023-02-26 16:28:14 -0500

    fetch data from github api using componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users')
+      .then((res) => console.log(res.data))
+  }
   render() {
     return (
       <div className='App'>
