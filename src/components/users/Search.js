ab4e84ac6aa23e8b4f3025d4b9b7e38b09369164
Noi-Git <noi.patsin@gmail.com>
2023-02-26 20:05:08 -0500

    modify onChange to be reusable by taget input name

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -5,6 +5,11 @@ export class Search extends Component {
     text: '',
   }
 
+  onChange = (e) => {
+    // this.setState({ text: e.target.value })
+    this.setState({ [e.target.name]: e.target.value }) // reuseable onChange - will target name that associated with each input name
+  }
+
   render() {
     return (
       <div>
@@ -14,10 +19,12 @@ export class Search extends Component {
             value={this.state.text}
             name='text'
             placeholder='Search users...'
+            onChange={this.onChange}
           />
           <input
             type='submit'
             value='Search'
+            name='submitSearch'
             className='btn btn-dark btn-block'
           />
         </form>
