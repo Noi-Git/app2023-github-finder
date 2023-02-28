78ded0e7dec75bb0f5cca8eb62cc5e84898173a9
Noi-Git <noi.patsin@gmail.com>
2023-02-28 17:03:23 -0500

    create fetch user repos function

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {}, // store a single user
+    repos: [],
     loading: false,
     alert: null,
   }
@@ -52,6 +53,17 @@ class App extends Component {
     )
     console.log('from getUser:-- ', res)
     this.setState({ user: res.data, loading: false })
+  }
+
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    console.log('from getUser:-- ', res)
+    this.setState({ repos: res.data, loading: false })
   }
 
   render() {
