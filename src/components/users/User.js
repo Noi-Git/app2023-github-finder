f544a4e14e9cbfb811fd6468f624c7ff4fc2363a
Noi-Git <noi.patsin@gmail.com>
2023-02-28 22:45:43 -0500

    convert to functional component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,105 +1,102 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import PropTypes from 'prop-types'
 import Repos from '../repos/Repos'
 
-export class User extends Component {
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.object.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-  }
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login)
+    getUserRepos(match.params.login)
+    // eslint-disable-next-line
+  }, [])
 
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login)
-    this.props.getUserRepos(this.props.match.params.login)
-  }
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user
 
-  render() {
-    const {
-      name,
-      company,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-    } = this.props.user
-
-    const { loading, repos } = this.props
-
-    if (loading) return <Spinner />
-    return (
-      <>
-        <Link to='/' className='btn btn-light'>
-          Back To Search
-        </Link>
-        Hireable:{' '}
-        {hireable ? (
-          <i className='fas fa-check text-success' />
-        ) : (
-          <i className='fas fa-fa-times-circle text-danger' />
-        )}
-        <div className='card grid-2'>
-          <div className='all-center'>
-            <img src={avatar_url} alt={name} style={{ width: '150px' }} />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </>
-            )}
-            <a href={html_url} className='btn btn-dark my-1'>
-              Visit GitHub Profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <p>
-                    <span className='text-bold'>Username:</span> {login}
-                  </p>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <p>
-                    <span className='text-bold'>Company:</span> {company}
-                  </p>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <p>
-                    <span className='text-bold'>Website:</span> {blog}
-                  </p>
-                )}
-              </li>
-            </ul>
-          </div>
+  if (loading) return <Spinner />
+  return (
+    <>
+      <Link to='/' className='btn btn-light'>
+        Back To Search
+      </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className='fas fa-check text-success' />
+      ) : (
+        <i className='fas fa-fa-times-circle text-danger' />
+      )}
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img src={avatar_url} alt={name} style={{ width: '150px' }} />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
         </div>
-        <div className='card text-center'>
-          <div className='badge badge-primary'>Followers: {followers}</div>
-          <div className='badge badge-success'>Following: {followers}</div>
-          <div className='badge badge-light'>Public Repos: {followers}</div>
-          <div className='badge badge-dark'>Public Gists: {followers}</div>
+        <div>
+          {bio && (
+            <>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            Visit GitHub Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <p>
+                  <span className='text-bold'>Username:</span> {login}
+                </p>
+              )}
+            </li>
+            <li>
+              {company && (
+                <p>
+                  <span className='text-bold'>Company:</span> {company}
+                </p>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <p>
+                  <span className='text-bold'>Website:</span> {blog}
+                </p>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </>
-    )
-  }
+      </div>
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-success'>Following: {following}</div>
+        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </>
+  )
+}
+
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.object.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
 }
 
 export default User
