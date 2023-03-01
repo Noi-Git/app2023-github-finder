fd1c9fba2e144576e73980e2bf00bc8df1266fcf
Noi-Git <noi.patsin@gmail.com>
2023-02-28 23:49:06 -0500

    setup searchUsers() to useContext

--- src/App.js
+++ src/App.js
@@ -27,17 +27,6 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false })
   // }
 
-  // Search Github users
-  const searchUsers = async (text) => {
-    setLoading(true)
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    )
-    setUsers(res.data.items)
-    setLoading(false)
-  }
-
   const clearUsers = () => {
     setUsers([])
     setLoading(false)
