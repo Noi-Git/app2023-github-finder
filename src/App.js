f289db7220613a8b4666e282ec2446f49a80f3c5
Noi-Git <noi.patsin@gmail.com>
2023-02-28 23:33:16 -0500

    wrap App.js with GithubState

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 import User from './components/users/User'
 
+import GithubState from './context/github/GithubState'
+
 const App = () => {
   const [users, setUsers] = useState([])
   const [user, setUser] = useState({})
@@ -70,46 +72,48 @@ const App = () => {
   }
 
   return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={alert} />
+    <GithubState>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={alert} />
 
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={(props) => (
-                <>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <>
+                    <Search
+                      searchUsers={searchUsers}
+                      clearUsers={clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={showAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </>
+                )}
+              />
+              <Route exact path='/about' element={<About />} />
+              <Route
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={getUser}
+                    getUserRepos={getUserRepos}
+                    user={user}
+                    repos={repos} //repos that is in the state
+                    loading={loading}
                   />
-                  <Users loading={loading} users={users} />
-                </>
-              )}
-            />
-            <Route exact path='/about' element={<About />} />
-            <Route
-              path='/user/:login'
-              render={(props) => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos} //repos that is in the state
-                  loading={loading}
-                />
-              )}
-            />
-          </Switch>
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </GithubState>
   )
 }
 
