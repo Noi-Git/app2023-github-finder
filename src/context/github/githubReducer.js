4ce643b0cc4fd6096076d48e68ef4977b171ad50
Noi-Git <noi.patsin@gmail.com>
2023-02-28 23:52:05 -0500

    setup searchUser in the reducer

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -7,7 +7,13 @@ import {
 } from '../types'
 
 export default (state, action) => {
-  switch (action.typ) {
+  switch (action.type) {
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      }
     case SET_LOADING:
       return {
         ...state,
